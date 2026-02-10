5d5ffc0f34073e38b04842e88f3d958dd3ed8f48
TrungDQ <trung.dq@qualgo.net>
2026-02-10 17:05:27 +0700

    update script

--- packages/vite-plugin-garfish/src/utils.ts
+++ packages/vite-plugin-garfish/src/utils.ts
@@ -15,14 +15,14 @@ export function scriptTransform(script$: Cheerio<Element>, publicPath: string) {
 
 export function reactHRMScriptTransfrom(script$: Cheerio<Element>, publicPath: string) {
   script$.removeAttr("type").html(`
-    ((window) => {
-      import("${publicPath}/@react-refresh").then(({default: RefreshRuntime}) => {
-        RefreshRuntime.injectIntoGlobalHook(window);
-        window.$RefreshReg$ = () => {};
-        window.$RefreshSig$ = () => (type) => type;
-        window.__vite_plugin_react_preamble_installed__ = true;
-      })
-    })(new Function("return this")());
+      ((window) => {
+        import("${publicPath}/@react-refresh").then(({default: RefreshRuntime}) => {
+          RefreshRuntime.injectIntoGlobalHook(window);
+          window.$RefreshReg$ = () => {};
+          window.$RefreshSig$ = () => (type) => type;
+          window.__vite_plugin_react_preamble_installed__ = true;
+        })
+      })(new Function("return this")());
   `);
 }
 
@@ -31,10 +31,15 @@ export function injectGarfishProvider(script$: Cheerio<Element>, publicPath?: st
   if (!src) return;
 
   script$.removeAttr("src").removeAttr("type").html(`
-    if (window.__GARFISH__ && typeof __GARFISH_EXPORTS__ === "object" && __GARFISH_EXPORTS__) {
-      __GARFISH_EXPORTS__.provider = new Promise((resolve) => {
-        import("${publicPath + src}").then(({provider}) => provider()).then((res) => resolve(res))
+    if (window.__GARFISH__ && typeof __GARFISH_EXPORTS__ === "object") {
+      let provider = new Promise((resolve) => {
+        import("${publicPath + src}").then(({provider}) => resolve(provider))
       })
+      if (__GARFISH_EXPORTS__.registerProvider) {
+        __GARFISH_EXPORTS__.registerProvider(provider);
+      } else {
+        __GARFISH_EXPORTS__.provider = provider;
+      }
     } else {
       import("${publicPath + src}")
     }
