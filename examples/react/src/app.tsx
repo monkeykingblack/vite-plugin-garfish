12190ebb15be686886055123f9c459d61b9fddd1
TrungDQ <trung.dq@qualgo.net>
2026-02-10 14:39:17 +0700

    fix wrong import naming

--- examples/react/src/app.tsx
+++ examples/react/src/app.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 import reactLogo from "./assets/react.svg";
 
-import "./App.css";
+import "./app.css";
 
 function App() {
   const [count, setCount] = useState(0);
